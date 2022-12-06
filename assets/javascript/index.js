91e4ba5f9bc97e2533643bdb0e96f56f9f997911
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2022-12-06 10:13:30 +0100

    add files

---
+++ assets/javascript/index.js
@@ -0,0 +1,16 @@
+const studente1 = { // oggetto
+    nome: "vincenzo", // proprietà 1 ---> stringa
+    cognome: "De Pascale" // proprietà 2 ---> stringa
+    età: 28, // proprietà 3 ---> numero
+    haLaWebcam: false, // proprietà 4 ---> booleano
+}
+
+// console.log(studente1) --> ti da tutte le proprietà
+// console.log(studente1.età) --> solo la proprietà selezionata
+
+const studente2 = { // oggetto
+    nome: "Mario", // proprietà 1 ---> stringa
+    cognome: "Campitiello" // proprietà 2 ---> stringa
+    età: 24, // proprietà 3 ---> numero
+    haLaWebcam: true, // proprietà 4 ---> booleano
+}
